73b3e44ff7f437b82e53d27196201c94d9b3abbc
anh-tran-codeleap <anh.tran@codeleap.de>
2026-04-09 12:10:20 +0700

    add Azure SignalR impl

--- Api/Program.cs
+++ Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR().AddAzureSignalR();
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddCors(options =>
